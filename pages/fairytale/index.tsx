a34154708dd6a32264fa36ea3628c362361a4bdb
Henrik WIlhelm Sissener <hws902@gmail.com>
2023-10-16 11:00:51 +0200

    More tweaks

--- pages/fairytale/index.tsx
+++ pages/fairytale/index.tsx
@@ -10,6 +10,7 @@ interface PageProps {
 }
 
 const StoriesPage = ({ fairytales }: PageProps) => {
+  const hasFairytales = fairytales.length > 0
   return (
     <>
       <main className="min-h-screen p-4">
@@ -17,7 +18,13 @@ const StoriesPage = ({ fairytales }: PageProps) => {
           <h1 className="bg-gradient-to-r from-pink-300 to-purple-600 bg-clip-text py-10 text-center font-extrabold text-transparent sm:text-7xl md:text-7xl lg:text-8xl xl:text-8xl">
             Eventyr
           </h1>
-          <div className="grid grid-cols-4 gap-4">
+          <div className="py-8 text-center">
+            <p>
+              This page will contain a list of all the fairytales that have been
+              created in Sanity.
+            </p>
+          </div>
+          <div className="grid grid-cols-4 gap-4 empty:hidden">
             {fairytales.map((fairytale, i) => (
               <Link href={`/fairytale/${fairytale.slug}`} key={i}>
                 <div className="relative aspect-1">
@@ -34,6 +41,11 @@ const StoriesPage = ({ fairytales }: PageProps) => {
               </Link>
             ))}
           </div>
+          {!hasFairytales && (
+            <p className="text-center">
+              <strong>There are no fairytales to display at the moment.</strong>
+            </p>
+          )}{' '}
         </section>
       </main>
     </>
@@ -45,6 +57,7 @@ export const getStaticProps: GetStaticProps<PageProps> = async () => {
   const [fairytales = []] = await Promise.all([getAllFairytales()])
 
   // Return the slugs for Next.js to use
+
   return {
     props: {
       fairytales: fairytales,
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,6 @@ export default function Page() {
   } else {
       console.log('error')
   }
-  setIsLoading(false)
 `
 
   const code2 = `
